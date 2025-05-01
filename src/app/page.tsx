68997c505cf281c2c2e76bfb796ffec8f0519108
Vinicius Benedito <beneditoloura@gmail.com>
2025-05-01 12:48:35 -0300

    feat: using component in landing page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,19 @@
+import CallToAction from "./_components/call-to-actions";
+import Features from "./_components/features";
+import Hero from "./_components/hero";
+import HowItWorks from "./_components/how-it-works";
+import Navbar from "./_components/navbar/navbar";
+import Showcase from "./_components/showcases";
+
 export default function Home() {
-  return <main>Landing Page</main>;
+  return (
+    <>
+      <Navbar />
+      <Hero />
+      <Features />
+      <HowItWorks />
+      <Showcase />
+      <CallToAction />
+    </>
+  );
 }
