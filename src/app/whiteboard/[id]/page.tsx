f3925710e7e92e4dbb24331cdfba13aa14712aad
Vinicius Benedito <beneditoloura@gmail.com>
2025-05-06 07:50:18 -0300

    refactor: improve whiteboard page

--- src/app/whiteboard/[id]/page.tsx
+++ src/app/whiteboard/[id]/page.tsx
@@ -1,12 +1,15 @@
 import { Timer } from "../_components/timer";
 import { ElementsSidebar } from "../_components/sidebar";
 import { Whiteboard } from "../_components/whiteboard";
-import { ChatInterface } from "../_components/interview-chat";
 import {
   ResizablePanelGroup,
   ResizablePanel,
   ResizableHandle,
 } from "@/components/ui/resizable";
+import { ChatContainer } from "../_components/interview-chat/chat-container";
+import { initialInterviewerMessage } from "@/usecases/interview-requirements/initialAIMessage";
+import { Message } from "../_components/interview-chat";
+import { unstable_cache } from "next/cache";
 
 export default async function Page({
   params,
@@ -17,13 +20,37 @@ export default async function Page({
 
   const clearId = id.replaceAll("-", " ");
 
+  const getProduct = unstable_cache(
+    async () => {
+      const initialMessage = await initialInterviewerMessage({
+        challengeName: id,
+        userLevel: "junior", // TODO: change to use from auth infos
+      });
+
+      return initialMessage;
+    },
+    ["junior", clearId],
+    {
+      tags: ["junior", clearId],
+      revalidate: 60 * 5,
+    }
+  );
+
+  const initialMessage = await getProduct();
+
+  const formattedInitialMessage: Message = {
+    challengeName: clearId,
+    id: crypto.randomUUID(),
+    text: initialMessage,
+    from: "interviewer" as Message["from"],
+  };
+
   return (
     <div className="h-screen w-full bg-background flex flex-col">
-      {/* Top bar with timer */}
-      <div className="h-14 border-b flex items-center px-4 justify-between">
+      {/* <div className="h-14 border-b flex items-center px-4 justify-between">
         <h1 className="text-lg font-semibold">{clearId}</h1>
         <Timer initialMinutes={45} />
-      </div>
+      </div> */}
 
       <div className="flex-1 flex overflow-hidden">
         <ResizablePanelGroup direction="horizontal">
@@ -46,8 +73,8 @@ export default async function Page({
 
               <ResizableHandle withHandle />
 
-              <ResizablePanel defaultSize={25} minSize={20} maxSize={40}>
-                <ChatInterface />
+              <ResizablePanel defaultSize={25} minSize={0} maxSize={40}>
+                <ChatContainer initialMessage={formattedInitialMessage} />
               </ResizablePanel>
             </ResizablePanelGroup>
           </ResizablePanel>
