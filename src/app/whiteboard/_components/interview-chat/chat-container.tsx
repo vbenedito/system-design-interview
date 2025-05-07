16daf64a5238361c9a3e8b8e46ebfaac305e8df5
Vinicius Benedito <beneditoloura@gmail.com>
2025-05-07 05:45:55 -0300

    refactor: adjust chatInterface component

--- src/app/whiteboard/_components/interview-chat/chat-container.tsx
+++ src/app/whiteboard/_components/interview-chat/chat-container.tsx
@@ -1,22 +1,49 @@
 // ChatContainer.tsx
 "use client";
 
+import { SendQuestionParams } from "@/usecases/ai-chat/api/sendMessage";
 import { ChatInterface, Message } from ".";
 import useAIChat from "@/hooks/aiChat/useAIChat";
+import { handleSendImage } from "@/lib/handleSendImage";
 
 export const ChatContainer = ({
   initialMessage,
+  challengeName,
+  userLevel,
 }: {
   initialMessage: Message;
+  challengeName: string;
+  userLevel: SendQuestionParams["userLevel"];
 }) => {
-  const { messages, setInput, input, handleSend } = useAIChat(initialMessage);
+  const {
+    messages,
+    setInput,
+    input,
+    loading,
+    setLoading,
+    handleSendMessage,
+    setMessages,
+  } = useAIChat({
+    initialMessage,
+    challengeName,
+    userLevel,
+  });
 
   return (
     <ChatInterface
       messages={messages}
+      loading={loading}
       input={input}
       onInputChange={setInput}
-      onSend={() => handleSend("senior")}
+      onSendMessage={() => handleSendMessage("senior")}
+      onSendImage={() =>
+        handleSendImage({
+          challengeName,
+          userLevel,
+          setMessages,
+          setLoading,
+        })
+      }
     />
   );
 };
