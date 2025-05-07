a396997e549c6efa5d3a9d0a3325642a7ca521d4
Vinicius Benedito <beneditoloura@gmail.com>
2025-05-07 05:44:11 -0300

    feat: add new redirect validations

--- src/middleware.ts
+++ src/middleware.ts
@@ -5,8 +5,18 @@ export function middleware(request: NextRequest) {
   const token = request.cookies.get("authjs.session-token");
   const pathname = request.nextUrl.pathname;
 
+  const whiteboardRegex = /^\/whiteboard\/[^/]+$/;
+
   if (pathname === "/auth" && token) {
     return NextResponse.redirect(new URL(getUrl("/app")));
+  }
+
+  if (pathname.includes("/whiteboard") && !whiteboardRegex.test(pathname)) {
+    return NextResponse.redirect(new URL(getUrl("/app/challenges")));
+  }
+
+  if (pathname.includes("/whiteboard") && !token) {
+    return NextResponse.redirect(new URL(getUrl("/auth")));
   }
 
   if (pathname.includes("/app") && !token) {
