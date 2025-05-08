4e8c18a7af70a6b9a9f1c0b608612136672bd14c
Vinicius Benedito <beneditoloura@gmail.com>
2025-05-08 06:20:20 -0300

    refactor: adjust settings page putting update user infos components

--- src/app/app/settings/(main)/page.tsx
+++ src/app/app/settings/(main)/page.tsx
@@ -1,3 +1,34 @@
+"use client";
+
+import { Button } from "@/components/ui/button";
+import { Card, CardHeader, CardTitle, CardContent } from "@/components/ui/card";
+
 export default function Page() {
-  return <h1>setting</h1>;
+  return (
+    <div className="flex items-center justify-center dark:from-brand-950/30 dark:via-background dark:to-background">
+      <div className="w-full space-y-8 animate-fade-in">
+        <Card>
+          <CardHeader>
+            <CardTitle>Professional Background</CardTitle>
+          </CardHeader>
+          <CardContent>
+            {/* <ProfessionalStep /> */}
+            professionalStep
+          </CardContent>
+        </Card>
+
+        <Card>
+          <CardHeader>
+            <CardTitle>Study History</CardTitle>
+          </CardHeader>
+          <CardContent>
+            {/* <StudyHistoryStep /> */}
+            studyHistory
+          </CardContent>
+        </Card>
+
+        <Button className="w-full">Save</Button>
+      </div>
+    </div>
+  );
 }
