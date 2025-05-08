91c1e3ed43404e860912a985e606172ee8c36a2c
Vinicius Benedito <beneditoloura@gmail.com>
2025-05-08 06:18:14 -0300

    refactor: adjust main sidebar

--- src/app/app/_components/main-sidebar.tsx
+++ src/app/app/_components/main-sidebar.tsx
@@ -11,11 +11,16 @@ import {
   DashboardSidebarHeader,
   DashboardSidebarFooter,
 } from "@/components/dashboard/sidebar";
-import { Settings, Swords } from "lucide-react";
+import { Settings, Swords, User } from "lucide-react";
 import { usePathname } from "next/navigation";
 import { UserInfos } from "./user-infos";
 import { Logo } from "@/components/logo";
 import { Session } from "next-auth";
+import {
+  Collapsible,
+  CollapsibleContent,
+  CollapsibleTrigger,
+} from "@/components/ui/collapsible";
 
 type Props = {
   user: Session["user"];
@@ -44,13 +49,30 @@ export function MainSidebar({ user }: Props) {
                 <Swords className="w-3 h-3 mr-3" />
                 Desafios
               </DashboardSidebarNavLink>
-              <DashboardSidebarNavLink
-                href="/app/settings"
-                active={isActive("/app/settings")}
-              >
-                <Settings className="w-3 h-3 mr-3" />
-                Configurações
-              </DashboardSidebarNavLink>
+              <Collapsible>
+                <CollapsibleTrigger className="flex items-center text-xs px-3 py-2 rounded-md">
+                  <Settings className="w-3 h-3 mr-3" />
+                  Configurações
+                </CollapsibleTrigger>
+                <CollapsibleContent className="ml-5">
+                  <DashboardSidebarNavLink
+                    href="/app/settings"
+                    active={isActive("/app/settings")}
+                  >
+                    <User className="w-3 h-3 mr-3" />
+                    Profile
+                  </DashboardSidebarNavLink>
+                </CollapsibleContent>
+                <CollapsibleContent className="ml-5">
+                  <DashboardSidebarNavLink
+                    href="/app/billing"
+                    active={isActive("/app/billing")}
+                  >
+                    <Settings className="w-3 h-3 mr-3" />
+                    Billing
+                  </DashboardSidebarNavLink>
+                </CollapsibleContent>
+              </Collapsible>
             </DashboardSidebarNavMain>
           </DashboardSidebarNav>
 
