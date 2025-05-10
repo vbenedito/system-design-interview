43b8c1c6624bf6045ac84417c7429ddd12ba55cf
Vinicius Benedito <beneditoloura@gmail.com>
2025-05-10 15:56:09 -0300

    refactor: remove void classname

--- src/app/auth/_components/authForm/auth-form.tsx
+++ src/app/auth/_components/authForm/auth-form.tsx
@@ -2,7 +2,7 @@ import LoginForm from "../loginForm/login-form";
 
 export function AuthForm() {
   return (
-    <div className="">
+    <div>
       <div className="flex flex-col gap-4 p-6 md:p-10">
         <div className="flex flex-1 items-center justify-center">
           <div className="w-full">
