5daf7aeb14b22069a7c0c9419e5007527f86f6f4
Vinicius Benedito <beneditoloura@gmail.com>
2025-05-11 15:40:05 -0300

    refactor: adjust user types

--- src/types/User.ts
+++ src/types/User.ts
@@ -1,3 +1,15 @@
 import { User } from "@prisma/client";
+import { OnboardingInfos } from "./Onboarding";
+import { Dispatch, SetStateAction } from "react";
 
 export type UserProps = User;
+
+export type ProfessionalStepProps = Pick<
+  UserProps,
+  "workInIndustry" | "yearsExperience" | "seniorityLevel"
+> & { setInfos: Dispatch<SetStateAction<OnboardingInfos>> };
+
+export type StudyHistoryStepProps = Pick<
+  UserProps,
+  "howMuchTimeStudySystemDesign" | "unity"
+> & { setInfos: Dispatch<SetStateAction<OnboardingInfos>> };
