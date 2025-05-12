fbcf896b15c7465bc10536717e9f894ddeab6ada
Vinicius Benedito <beneditoloura@gmail.com>
2025-05-12 07:18:39 -0300

    refactor: remove authentication config

--- src/service/auth/index.ts
+++ src/service/auth/index.ts
@@ -19,13 +19,6 @@ export const {
     Google({
       clientId: process.env.AUTH_GOOGLE_ID || "",
       clientSecret: process.env.AUTH_GOOGLE_SECRET || "",
-      authorization: {
-        params: {
-          prompt: "consent",
-          access_type: "offline",
-          response_type: "code",
-        },
-      },
     }),
   ],
 });
