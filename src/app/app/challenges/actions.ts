19fa0f9c109b3a3bcb34293a88289eccb729f456
Vinicius Benedito <beneditoloura@gmail.com>
2025-05-13 06:13:46 -0300

    feat: add action to create a challenge

--- src/app/app/challenges/actions.ts
+++ src/app/app/challenges/actions.ts
@@ -1,7 +1,19 @@
 import { prisma } from "@/service/database";
+import { Challenge } from "@/types/Challenge";
 
 export async function getChallenges() {
   const challenges = await prisma.challenge.findMany();
 
   return challenges;
+}
+
+export async function createChallenge(challengeInfos: Challenge) {
+  await prisma.challenge.create({
+    data: {
+      title: challengeInfos.title,
+      description: challengeInfos.description,
+      slug: challengeInfos.slug,
+      difficulty: challengeInfos.difficulty,
+    },
+  });
 }
