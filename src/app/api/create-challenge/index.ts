cb4a43065d07c55aa8b617a51699e06d7780415c
Vinicius Benedito <beneditoloura@gmail.com>
2025-05-13 06:19:36 -0300

    feat: add router to create a challenge

---
+++ src/app/api/create-challenge/index.ts
@@ -0,0 +1,25 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import { NextRequest, NextResponse } from "next/server";
+import { prisma } from "@/service/database";
+
+export async function POST(req: NextRequest) {
+  try {
+    const body = await req.json();
+
+    const { title, description, slug, difficulty } = body;
+
+    const novoProduto = await prisma.challenge.create({
+      data: {
+        title: title,
+        description: description,
+        slug: slug,
+        difficulty: difficulty,
+      },
+    });
+
+    return NextResponse.json(novoProduto);
+  } catch (error: any) {
+    console.error(error);
+    return NextResponse.json({ error: error.message }, { status: 500 });
+  }
+}
