b5003f04bf2f7fef3849de49d08be67b0202ccca
Vinicius Benedito <beneditoloura@gmail.com>
2025-05-13 06:16:53 -0300

    refactor: remove action

--- src/app/app/challenges/actions.ts
+++ src/app/app/challenges/actions.ts
@@ -1,19 +1,7 @@
 import { prisma } from "@/service/database";
-import { Challenge } from "@/types/Challenge";
 
 export async function getChallenges() {
   const challenges = await prisma.challenge.findMany();
 
   return challenges;
-}
-
-export async function createChallenge(challengeInfos: Challenge) {
-  await prisma.challenge.create({
-    data: {
-      title: challengeInfos.title,
-      description: challengeInfos.description,
-      slug: challengeInfos.slug,
-      difficulty: challengeInfos.difficulty,
-    },
-  });
 }
